80009f08401554d3d1a771e93afb61c6b86c67a4
minseok <bbb9316@naver.com>
2024-02-14 01:55:35 +0900

    2024-02-14 me api 연동
    - 로그인, 로그아웃 구현

--- client/src/pages/auth/login/login.js
+++ client/src/pages/auth/login/login.js
@@ -1,6 +1,6 @@
-import AuthService from "../auth.js";
-import HttpClient from "../../../utils/network/http.js";
-import TokenStorage from "../../../utils/db/token.js";
+import AuthService from "/client/src/pages/auth/auth.js";
+import HttpClient from "/client/src/utils/network/http.js";
+import TokenStorage from "/client/src/utils/db/token.js";
 
 const authService = new AuthService(
   new HttpClient("http://127.0.0.1:8080"),
